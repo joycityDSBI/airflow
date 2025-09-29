694fb4579b2ab85f99070024ab5fd4966004df3b
seongin <seongin@joycity.com>
2025-09-29 15:25:53 +0900

    changed email

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -12,7 +12,7 @@ with DAG (
     send_email_task = EmailOperator (
         task_id='send_email_task',
         conn_id = 'conn_smtp_gmail',
-        to='seongin@joycity.com',
+        to='65e43b85.joycity.com@kr.teams.ms',
         subject='Airflow 성공메일',
         html_content = 'Airflow 작업이 완료되었습니다.'
     )
