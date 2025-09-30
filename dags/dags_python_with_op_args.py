4d6eaab5f9e51f3a0293f29039a2384dbff640fc
seongin <seongin@joycity.com>
2025-09-30 14:33:29 +0900

    regist add

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from common.common_func import get_sftp
+from common.common_func import get_sftp, regist
 
 with DAG(
     dag_id = "dags_python_import_func",
@@ -11,8 +11,11 @@ with DAG(
     catchup = False
 ) as dag:
     
-    task_get_sftp = PythonOperator(
-        task_id = 'task_get_sftp',
-        python_callable = get_sftp
+    regist_t1 = PythonOperator(
+        task_id = 'regist_t1',
+        python_callable = regist,
+        op_args = ['홍길동', '남자', '30세', '서울시 강남구', '010-1234-5678']
     )
+
+    regist_t1
     
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,2 +1,12 @@
 def get_sftp():
     print('sftp 작업을 시작합니다.')
+
+
+def regist(name, sex, *args):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    for arg in args:
+        print(f'추가 정보: {arg}')
+
+
+        
