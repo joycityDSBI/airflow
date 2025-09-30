af951b1f6a318ca2b464b6ce8a5fe3a372571c4e
seongin <seongin@joycity.com>
2025-09-30 14:44:39 +0900

    수정

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import get_sftp, regist
 
 with DAG(
-    dag_id = "dags_python_import_func",
+    dag_id = "dags_python_with_op_args",
     schedule = "30 6 * * *",
     start_date = pendulum.datetime(2025, 9, 30, tz="Asia/Seoul"),
     catchup = False
