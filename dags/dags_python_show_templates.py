2313bc55d08be5b04f7e61f444f78c5c5263a46a
seongin <seongin@joycity.com>
2025-09-30 15:44:11 +0900

    templates update

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id = "dags_python_show_templates",
+    schedule = "30 6 * * *",
+    start_date = pendulum.datetime(2025, 9, 20, tz="Asia/Seoul"),
+    catchup = True
+) as dag:
+    
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
+
+    
