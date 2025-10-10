b83f2cbc4eb72a5f3c3a16b246e8e40ec9c09bc6
seongin <seongin@joycity.com>
2025-10-10 14:07:28 +0900

    edit hook

---
+++ dags/dags_python_with_postgres_hook.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id = 'dags_python_with_postgres',
+    description = 'A simple DAG to test PythonOperator with Postgres',
+    schedule = None,
+    start_date = datetime(2025, 9, 15),
+    catchup = False,
+) as dag:
+
+
+    def insrt_postgres(postgres_conn_id, **kwargs):
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+        from contextlib import closing
+
+        postgres_hook = PostgresHook(postgres_conn_id)
+        with closing(postgres_hook.get_conn()) as conn:
+            with closing(conn.cursor()) as cursor:
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
+                msg = 'insert 수행'
+                sql = 'insert into py_opr_drct_insrt values (%s, %s, %s, %s);'
+                cursor.execute(sql, (dag_id, task_id, run_id, msg))
+                conn.commit()
+
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres_with_hook',
+        python_callable=insrt_postgres,
+        op_args={'postgres_conn_id': 'conn_db_postgres_custom'}
+    )
+    
+
+    insrt_postgres
