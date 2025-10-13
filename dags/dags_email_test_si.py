edacf352889823eb482f35caf1d217a495c209fb
seongin <seongin@joycity.com>
2025-10-13 09:44:38 +0900

    email test edit

--- dags/dags_email_test_si.py
+++ dags/dags_email_test_si.py
@@ -4,7 +4,7 @@ import datetime
 from airflow.operators.email import EmailOperator
 
 with DAG (
-    dag_id = "dags_email_operator",
+    dag_id = "dags_email_test_si",
     schedule = "0 8 1 * *",
     start_date = pendulum.datetime(2025, 9, 29, tz="Asia/Seoul"),
     catchup = False
@@ -16,4 +16,3 @@ with DAG (
         subject='Airflow 성공메일',
         html_content = 'Airflow 작업이 완료되었습니다.'
     )
-    
