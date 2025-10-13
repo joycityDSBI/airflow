8eef25a2730511e5fa38abb976dbef30581e9d2c
seongin <seongin@joycity.com>
2025-10-13 09:37:38 +0900

    add email test py

---
+++ dags/dags_timeout_example_1.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+import pendulum
+from datetime import timedelta
+from airflow.models import Variable
+
+email_str = Variable.get("email_target")
+email_list = [email.strip() for email in email_str.split(',')]
+
+with DAG (
+    dag_id = 'dags_timeout_example_1',
+    description = 'A simple DAG to demonstrate task timeouts',
+    catchup = False,
+    schedule = None,
+    drgrun_timeout = timedelta(minutes=1),
+    default_args={
+        'execution_timeout': timedelta(seconds=20),
+        'email_on_failure': True,
+        'email': email_list,
+    }
+) as dag:
+    
+    bash_sleep_30 = BashOperator(
+        task_id = 'bash_sleep_30',
+        bash_command = 'sleep 30'
+    )
+
+    bash_sleep_10 = BashOperator(
+        task_id = 'bash_sleep_10',
+        bash_command = 'sleep 10'
+    )
+
+    bash_sleep_30 >> bash_sleep_10
