1fcc763509f5c76069cfd6ca8f7c8df06b62d56c
joycityDSBI <ds_bi@joycity.com>
2025-10-22 15:41:33 +0900

    edit notion master list

--- dags/notiondb_bq.py
+++ dags/notiondb_bq.py
@@ -155,6 +155,8 @@ def load_to_bigquery(**context):
     """BigQuery 적재"""
     df = df
 
+    print(df['Package_Kind'].head())
+
     df['Start_Date'] = pd.to_datetime(df['Start_Date'], errors='coerce').dt.date
     df['End_Date'] = pd.to_datetime(df['End_Date'], errors='coerce').dt.date
     
