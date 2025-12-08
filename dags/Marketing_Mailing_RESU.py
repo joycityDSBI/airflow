4304e569eb4bfa3759ffe9e05731a5de09585603
joycityDSBI <ds_bi@joycity.com>
2025-12-08 18:18:25 +0900

    edit password

--- dags/Marketing_Mailing_RESU.py
+++ dags/Marketing_Mailing_RESU.py
@@ -932,7 +932,7 @@ with DAG(
 
         except Exception as e:
             logger.error(f"❌ 에러 발생: {str(e)}", exc_info=True)
-            print(f"❌ 에러 발생: {str(e)}"
+            print(f"❌ 에러 발생: {str(e)}")
             raise
 
     # Task 정의
