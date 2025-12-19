80ced89ca657cc4bf7b73de573d237f3010bf9a1
unknown <qhrua101@joycity.com>
2025-12-19 18:28:51 +0900

    etl_dim_AFC_campaign edit

--- dags/ETL_dimension.py
+++ dags/ETL_dimension.py
@@ -98,109 +98,81 @@ with DAG(
         """
 
         query = f"""
-        INSERT INTO `datahub-478802.datahub.dim_AFC_campaign'
+        INSERT INTO  `datahub-478802.datahub.dim_AFC_campaign`
+        
         (app_id, media_source, init_campaign, Gcat, uptdt_campaign, media_category, product_category, media, media_detail, optim, etc_category, os_cam, geo_cam, date_cam, 
-        creative_no, device, setting_title, landing_title, ad_unit, mediation, create_YN, update_YN, rule_YN, class, target_group, media_group, country_group, create_timestamp)
+        creative_no, device, setting_title, landing_title, ad_unit, mediation, create_YN, update_YN, rule_YN, target_group, media_group, upload_timestamp)
+        
         SELECT DISTINCT
-            app_id                                  AS AppID
-            , NORMALIZE(media_source, NFC)            AS MediaSource
-            , NORMALIZE(init_campaign, NFC)           AS InitCampaign
-            , gcat                                    AS GCat
-            , NORMALIZE(uptdt_campaign, NFC)          AS UptdtCampaign
-            , media_category                          AS MediaCategory
-            , product_category                        AS ProductCategory
-            , media                                   AS Media
-            , media_detail                            AS MediaDetail
-            , optim                                   AS Optim
-            , etc_category                            AS EtcCategory
-            , os_cam                                  AS OSCam
-            , geo_cam                                 AS GEOCam
-            , date_cam                                AS DateCam
-            , creative_no                             AS CreativeNo
-            , device                                  AS Device
-            , setting_title                           AS SettingTitle
-            , landing_title                           AS LandingTitle
-            , ad_unit                                 AS ADUnit
-            , mediation                               AS Mediation
-            , create_yn                               AS CreateYN
-            , update_yn                               AS UpdateYN
-            , rule_yn                                 AS RuleYN
-            , class                                   AS Class
-            , CASE
-                WHEN 
-                    etc_category = 'L&F' THEN '6.ETC'  
-                WHEN 
-                    (media_category IN ('ADNW','AppleSA.Self') AND media NOT IN ('Mistplay') AND class LIKE '%tROAS%') OR
-                    (media_category IN ('Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re' ,'Mytarget.Self') AND class in('VO','VO(MinROAS)') ) OR
-                    (media_category IN ('Google','Google-Re') AND etc_category NOT IN ('GoldAccel') AND class LIKE '%tROAS%') OR
-                    (media_category IN ('Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re' ,'Mytarget.Self', 'Google', 'Google-Re') AND class IN ('CEO(HVU)','GFS(HVU)') ) THEN '1.UA-HVU'
-                WHEN 
-                    (media_category IN ('ADNW','AppleSA.Self') AND (class LIKE '%tRET%' OR class LIKE '%tROAS%' OR class IN('CPA','CPC','CPM', 'CPC_Retargeting', 'CPM_AEO(Level)','CPM_AEO(Purchase)','CPM_CEO(Pecan)' )) ) OR 
-                    (media_category in( 'Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re' ,'Mytarget.Self') AND class in('AEO(Level)','AEO','AEO(Gem)','CEO','CEO(Pecan)','CEO(Model)','CEO(Action)') ) OR
-                    (media_category IN ('Google','Google-Re') AND (class in('tCPA_ETC','tCPA_Purchase','tCPA','tCPA(GFS)_ETC') OR class LIKE '%tROAS%')) OR
-                    (media_category IN ( 'Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re' ,'Mytarget.Self','Google', 'Google-Re') AND class IN ('CEO(VU)','GFS(VU)') ) THEN '2.UA-VU'
-                WHEN 
-                    (media_category IN ('ADNW','AppleSA.Self' )  AND (class IN ('nCPI','nCPA') OR class LIKE '%tCPI%')) OR
-                    (media_category IN ('Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re' ,'Mytarget.Self') AND class in('MAIA','MAIE' ,'Linkclick','NONE') ) OR
-                    (media_category IN ('Google','Google-Re') AND class in('tCPI_ETC','tCPI','tCPI-Adv_ETC','tCPI-Adv_Purchase','NONE','ARO') ) OR 
-                    (media_category IN ('Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re' ,'Mytarget.Self','Google', 'Google-Re')) AND class IN ('CEO(Install)','GFS(Install)') THEN '3.UA-Install'
-                WHEN 
-                    media_category = 'Restricted' THEN '4.UA-Restricted'
-                WHEN 
-                    gcat IN ('Organic','Unknown') THEN '5.Organic'
-                ELSE '6.ETC'
-            END                                      AS TargetGroup
-            , CASE WHEN media_category = 'Google'                 THEN 'Google'
-                    WHEN media_category = 'Facebook'               THEN 'FB'
-                    WHEN media_category = 'ADNW'                   THEN 'ADNW'
-                    WHEN LOWER(gcat) IN ('organic','unknown')      THEN 'Organic'
-                    ELSE 'Other' 
-            END                                     AS MediaGroup
-            , upload_time                              AS UploadTimestamp  
-        FROM (
-        SELECT app_id
-            , CASE WHEN LOWER(TRIM(media_source)) = 'organic'   THEN 'Organic'
-                    ELSE gcat
-                END AS gcat
-            , media_category
-            , product_category
-            , media
-            , media_detail
-            , CASE WHEN LOWER(TRIM(media_source)) = 'organic'   THEN 'Organic'
-                    ELSE TRIM(media_source) 
-                END AS media_source
-            , optim
-            , etc_category
-            , os                                  AS os_cam
-            , IF(location = 'UK', 'GB', location) AS geo_cam
-            , cmpgn_dt                            AS date_cam
-            , creative_no 
-            , device
-            , setting_title
-            , landing_title
-            , ad_unit
-            , mediation
-            , create_yn
-            , update_yn
-            , rule_yn
-            , init_campaign
-            , uptdt_campaign
-            , CASE
-                    WHEN etc_category   = 'L&F'                                                                              THEN CONCAT(optim,'_L&F')
-                    WHEN media_category = 'ADNW' AND (optim IS NULL OR optim = '구분없음')                                    THEN product_category
-                    WHEN media_category = 'ADNW'                                                                             THEN CONCAT(product_category,'_',optim)
-                    WHEN media_category IN ('Facebook' ,'Facebook-Gaming','Facebook-Playable','Facebook-Re')                 THEN optim        
-                    WHEN media_category = 'AppleSA.Self'                                                                     THEN product_category
-                    WHEN media_category = 'Mytarget.Self'                                                                    THEN optim
-                    WHEN media_category IN ('Google','Google-Re') AND (etc_category IS NULL OR etc_category = '구분없음')     THEN optim
-                    WHEN media_category IN ('Google','Google-Re') AND etc_category =  'Purchase'                             THEN CONCAT(optim,'_',etc_category)
-                    WHEN media_category IN ('Google','Google-Re') AND etc_category != 'Purchase'                             THEN CONCAT(optim,'_ETC')
-                    ELSE '구분없음'
-                END AS class
-            , upload_time       
-        FROM `dataplatform-bdts.mas.af_campaign_rule`
-        where 
-        ) AS a
+                      app_id                                    
+                    , NORMALIZE(media_source, NFC)            AS media_source
+                    , NORMALIZE(init_campaign, NFC)           AS init_campaign
+                    , gcat                                    AS gcat
+                    , NORMALIZE(uptdt_campaign, NFC)          AS uptdt_campaign
+                    , media_category                          AS media_category
+                    , product_category                        AS product_category
+                    , media                                   AS media
+                    , media_detail                            AS media_detail
+                    , optim                                   AS optim
+                    , etc_category                            AS etc_category
+                    , os_cam                                  AS os_cam
+                    , geo_cam                                 AS geo_cam
+                    , date_cam                                AS date_cam
+                    , creative_no                             AS creative_no
+                    , device                                  AS device
+                    , setting_title                           AS setting_title
+                    , landing_title                           AS landing_title
+                    , ad_unit                                 AS ad_unit
+                    , mediation                               AS mediation
+                    , create_yn                               AS create_yn
+                    , update_yn                               AS update_yn
+                    , rule_yn                                 AS rule_yn
+                    , target_group                            AS target_group
+                    , CASE WHEN media_category in ('Google', 'Google-ACP', 'Google-PC', 'Google-Re') THEN 'Google'
+                            WHEN media_category in ('Facebook', 'Facebook-3rd', 'Facebook-Gaming', 'Facebook-PC', 'Facebook-Playable', 'Facebook-Re') THEN 'FB'
+                            WHEN media_category in ('ADNW','ADNW-Re')                   THEN 'ADNW'
+                            WHEN LOWER(gcat) in ('organic','unknown')   THEN 'Organic'
+                            ELSE 'Other' 
+                     END AS media_group  -- 각 빅미디어 모든 매체카테고리 추가
+                    , upload_time                              AS upload_timestamp  
+                FROM (
+                SELECT app_id
+                    , CASE WHEN LOWER(TRIM(media_source)) = 'organic' THEN 'Organic' ELSE gcat END AS gcat
+                    , media_category
+                    , product_category
+                    , media
+                    , media_detail
+                    , CASE WHEN LOWER(TRIM(media_source)) = 'organic' THEN 'Organic'
+                           ELSE TRIM(media_source) END AS media_source
+                    , optim
+                    , etc_category
+                    , os                                  AS os_cam
+                    , IF(location = 'UK', 'GB', location) AS geo_cam
+                    , cmpgn_dt                            AS date_cam
+                    , creative_no 
+                    , device
+                    , setting_title
+                    , landing_title
+                    , ad_unit
+                    , mediation
+                    , create_yn
+                    , update_yn
+                    , rule_yn
+                    , init_campaign
+                    , uptdt_campaign
+                    , upload_time
+                    , case 
+                           when etc_category = 'L&F' then '그룹없음'
+                           when (media_category = 'Mytarget.Self' and gcat = 'UA' and product_category is null and optim = 'MAIA') then 'UA-Install'
+                           when lower(trim(media_source)) = 'organic' then 'Organic'
+                           -- 현재 모두 install로 남고 있지만 데이터 수정하면 제대로 남을 예정 - ad_name 컬럼이 없어서 대응을 못함.
+                           --    when (media_category = 'Facebook' and gcat = 'UA' and product_category is null and optim = 'NONE' and optim2 = 'VO') then 'UA-HVU'
+                           --    when (media_category = 'Facebook' and gcat = 'UA' and product_category is null and optim = 'NONE' and optim2 = 'MAIA') then 'UA-Install'
+                           --    when (media_category = 'Facebook' and gcat = 'UA' and product_category is null and optim = 'NONE' and optim2 = 'AEO') then 'UA-VU' 
+                           else target_group
+                      end as target_group -- 데이터 처리 전까지만 하드코딩 대응 수정된 이후에 하드코딩은 삭제 예정       
+                FROM `dataplatform-bdts.mas.v_af_campaign_rule_group`
+                ) 
         """
 
         client.query(truncate_query)
