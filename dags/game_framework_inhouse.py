5b2a1a549fea6de822435ffe4eaff4f1010f0418
joycityDSBI <ds_bi@joycity.com>
2026-01-12 17:28:41 +0900

    loger delete

--- dags/game_framework_inhouse.py
+++ dags/game_framework_inhouse.py
@@ -56,6 +56,10 @@ from game_framework_util import *
 PROJECT_ID = "data-science-division-216308"
 LOCATION = "us-central1"
 
+import logging
+
+# matplotlib.category의 로그 레벨을 WARNING으로 설정 (INFO 로그 무시)
+logging.getLogger('matplotlib.category').setLevel(logging.WARNING)
 
 ## 한글 폰트 설정
 setup_korean_font()
@@ -143,9 +147,6 @@ def inhouse_sales_before24_query(joyplegameid: int, gameidx: str, bigquery_clien
 ## 제미나이 프롬프트 
 def inhouses_revenue_gemini(gameidx:str, service_sub: str, genai_client, MODEL_NAME, SYSTEM_INSTRUCTION:list, path_daily_revenue, path_monthly_revenue, bucket, PROJECT_ID, LOCATION, **context):
     
-    from google.genai import Client
-    genai_client = Client(vertexai=True,project=PROJECT_ID,location=LOCATION)
-
     inhouse_sales = load_df_from_gcs(bucket, path_daily_revenue)
     inhouse_sales_before24 = load_df_from_gcs(bucket, path_monthly_revenue)
 
