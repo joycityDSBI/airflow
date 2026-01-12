d92bbbf27b6421f06b820104de65f48e8fafc813
joycityDSBI <ds_bi@joycity.com>
2026-01-13 08:53:21 +0900

    edit main framework schedule

--- dags/game_framework_gbtw_main.py
+++ dags/game_framework_gbtw_main.py
@@ -512,30 +512,30 @@ with DAG(
         python_callable=make_gameframework_notion_page_task,
     )
 
-    t2_daily = PythonOperator(
-        task_id='daily_data_game_framework',
-        python_callable=daily_data_task,
-    )
+    # t2_daily = PythonOperator(
+    #     task_id='daily_data_game_framework',
+    #     python_callable=daily_data_task,
+    # )
 
-    t3_inhouse = PythonOperator(
-        task_id='inhouse_data_game_framework',
-        python_callable=inhouse_data_task,
-    )
+    # t3_inhouse = PythonOperator(
+    #     task_id='inhouse_data_game_framework',
+    #     python_callable=inhouse_data_task,
+    # )
 
-    t4_global_ua = PythonOperator(
-        task_id='global_ua_data_game_framework',
-        python_callable=global_ua_data_task,
-    )
+    # t4_global_ua = PythonOperator(
+    #     task_id='global_ua_data_game_framework',
+    #     python_callable=global_ua_data_task,
+    # )
 
-    t5_rgroup = PythonOperator(
-        task_id='rgroup_iapgemruby_data_game_framework',
-        python_callable=rgroup_iapgemruby_task,
-    )
+    # t5_rgroup = PythonOperator(
+    #     task_id='rgroup_iapgemruby_data_game_framework',
+    #     python_callable=rgroup_iapgemruby_task,
+    # )
 
-    t6_longterm = PythonOperator(
-        task_id='longterm_sales_data_game_framework',
-        python_callable=longterm_sales_task,
-    )
+    # t6_longterm = PythonOperator(
+    #     task_id='longterm_sales_data_game_framework',
+    #     python_callable=longterm_sales_task,
+    # )
 
     t7_newuser_roas = PythonOperator(
         task_id='newuser_roas_data_game_framework',
