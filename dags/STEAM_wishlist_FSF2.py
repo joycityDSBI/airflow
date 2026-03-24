1f7d422597ca104f99907721cc9647498610ef52
Seongin <seongin@joycity.com>
2026-03-24 17:08:22 +0900

    edit requirements

--- dags/STEAM_wishlist_FSF2.py
+++ dags/STEAM_wishlist_FSF2.py
@@ -331,8 +331,11 @@ def steam_follower_etl():
         url = f"https://store.steampowered.com/api/requestedcounts?ids={app_id}"
 
         headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
-        "referer": f"https://partner.steampowered.com/app/wishlist/{app_id}/"
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache'
         }
 
         try:
@@ -466,4 +469,4 @@ with DAG(
     )
 
 
-    steam_follower_etl_task >>upload_to_bigquery_task >> upload_to_notion_task 
+    upload_to_bigquery_task >> upload_to_notion_task >> steam_follower_etl_task
